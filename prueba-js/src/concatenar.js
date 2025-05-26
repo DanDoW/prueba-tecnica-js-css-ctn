21c01fece7bc06c92d94dcf84e50646241e7ec1d
Daniel Rodriguez Gargallo <dani.rodriguez.gargallo@gmail.com>
2025-05-26 10:11:24 +0200

    Adds Concatenar function

--- prueba-js/src/concatenar.js
+++ prueba-js/src/concatenar.js
@@ -1,5 +1,34 @@
+const capitalizar = (str) => {
+  if (!str || typeof str !== "string") {
+    return;
+  }
+
+  let name = str.toLowerCase().split(" ");
+
+  let capitalizedName = name.map(
+    (singledName) => singledName.charAt(0).toUpperCase() + singledName.slice(1)
+  );
+
+  return capitalizedName.join(" ");
+};
+
 export default (json) => {
-    // Do your thing here!
-    return "";
-  };
-  
+  return json
+    .map((user) => {
+      const { nombre, apellido, apellido2 } = user;
+      let completeName = [];
+
+      if (!!nombre) {
+        completeName.push(capitalizar(nombre));
+      }
+      if (!!apellido) {
+        completeName.push(capitalizar(apellido));
+      }
+      if (!!apellido2) {
+        completeName.push(capitalizar(apellido2));
+      }
+
+      return completeName.join(" ");
+    })
+    .join("<br>");
+};
