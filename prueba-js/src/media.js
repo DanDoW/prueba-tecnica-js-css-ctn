fd13a7fdd4c74dbc5a9b1b73c1969dd1b8c17338
Daniel Rodriguez Gargallo <dani.rodriguez.gargallo@gmail.com>
2025-05-26 10:40:41 +0200

    Adds Media function

--- prueba-js/src/media.js
+++ prueba-js/src/media.js
@@ -1,5 +1,17 @@
 export default (json) => {
-    // Do your thing here!
-    return "";
-  };
-  
+  let exclude = 0;
+  const total = json.reduce((accumulator, user) => {
+    const { edad } = user;
+    if (typeof edad !== "number" || edad < 0) {
+      exclude++;
+      return accumulator;
+    }
+    return accumulator + edad;
+  }, 0);
+
+  if (exclude == json.length) {
+    return "No hay edades válidas";
+  }
+
+  return total / (json.length - exclude);
+};
